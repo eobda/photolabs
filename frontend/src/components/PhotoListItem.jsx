6b0e7e07134e12a1db4964f7a21f8b0771ec58cb
maddie <lee.maddie@gmail.com>
2023-12-18 14:46:09 -0500

    Move sample data to App.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,8 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  const photoData = sampleDataForPhotoListItem;
+const PhotoListItem = (props) => {
+  const photoData = props.photoData;
   return (
     <div>
         <img src ={photoData.imageSource} />
