39e413fc446ab1c9c8a224cab7d2b6773aadc44c
maddie <lee.maddie@gmail.com>
2023-12-19 13:20:36 -0500

    Add PhotoFavIcon to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   const photoData = props.photoData;
   
   return (
     <div className="photo-list__item">
-        <img className="photo-list__image" src ={photoData.imageSource} />
-        <div className="photo-list__user-details">
-          <img className="photo-list__user-profile" src={photoData.profile} />
-          <p className="photo-list__user-info">{photoData.username}<br />
-          <span className="photo-list__user-location">{`${photoData.location.city}, ${photoData.location.country}`}</span></p>          
-        </div>
+      <PhotoFavButton />
+      <img className="photo-list__image" src ={photoData.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={photoData.profile} />
+        <p className="photo-list__user-info">{photoData.username}<br />
+        <span className="photo-list__user-location">{`${photoData.location.city}, ${photoData.location.country}`}</span></p>          
+      </div>
     </div>
   );
 };
