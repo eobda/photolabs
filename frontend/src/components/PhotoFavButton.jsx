57dea40fef23feec15ba19306c99185a6a07e8cc
maddie <lee.maddie@gmail.com>
2023-12-18 19:15:34 -0500

    Add onClick function

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,9 +3,14 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
+const handleClick = function () {
+  console.log("click successful");
+};
+
 function PhotoFavButton() {
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
         <FavIcon />
       </div>
