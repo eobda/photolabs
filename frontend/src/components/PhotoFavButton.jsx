0841c3af165ec982a6b204c84b239967e78e1aab
maddie <lee.maddie@gmail.com>
2023-12-18 19:26:08 -0500

    Move state hooks to inside function

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,13 +3,13 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-const [selected, setSelected] = useState(false);
-const handleClick = () => {
-  setSelected(selected ? false : true)
-};
-
 function PhotoFavButton() {
 
+  const [selected, setSelected] = useState(false);
+  const handleClick = () => {
+    setSelected(selected ? false : true)
+  };
+  
   return (
     <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
