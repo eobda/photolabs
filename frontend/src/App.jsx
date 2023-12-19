bf5214b790fb5c76f2a81e90911761aa6a501ff7
maddie <lee.maddie@gmail.com>
2023-12-19 15:32:34 -0500

    Render HomeRoute on App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
-// import PhotoList from 'components/PhotoList';
 import './App.scss';
-import PhotoFavButton from 'components/PhotoFavButton';
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoFavButton />
+      <HomeRoute />
     </div>
   );
 };
