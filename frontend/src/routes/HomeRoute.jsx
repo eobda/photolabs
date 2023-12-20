0dc04c1272926fe10d0fc55464d9acba923634cc
maddie <lee.maddie@gmail.com>
2023-12-20 15:49:18 -0500

    Add liked state to parent

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
 const HomeRoute = () => {
+  const [liked, setLiked] = useState(false);
+
   return (
     <div className="home-route">
-      <TopNavigation />
-      <PhotoList />
+      <TopNavigation liked={liked} setLiked={setLiked} />
+      <PhotoList liked={liked} setLiked={setLiked} />
     </div>
   );
 };
