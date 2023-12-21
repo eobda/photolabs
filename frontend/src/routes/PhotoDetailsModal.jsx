7d221e9f49a291941af4975dd987ee296ac086e9
maddie <lee.maddie@gmail.com>
2023-12-21 11:41:33 -0500

    Fix styling

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -9,21 +9,26 @@ const PhotoDetailsModal = (props) => {
   const { activePhoto, setModalState } = props;
   return (
     <div className="photo-details-modal">
+
       <button className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" onClick={() => setModalState(null)} />
       </button>
-      <div className='photo-details-modal__top-bar'>
+
+      <div className='photo-details-modal__header'>
         <PhotoFavButton likeItem={null} photo={activePhoto} />
-        <img className='photo-details-modal__image' src={activePhoto.urls.regular} />
-        <div className='photo-details-modal__header'>
+        <img className='photo-details-modal__image' src={activePhoto.urls.full} />
+
+        <div className='photo-details-modal__photographer-details'>
           <img className="photo-details-modal__photographer-profile" src={activePhoto.user.profile} />
           <p className="photo-details-modal__photographer-info">{activePhoto.user.username}<br />
           <span className="photo-details-modal__photographer-location">{`${activePhoto.location.city}, ${activePhoto.location.country}`}</span></p> 
         </div>
       </div>
+        
       <div className='photo-details-modal__images'>
         <PhotoList />
       </div>
+
     </div>
   )
 };
