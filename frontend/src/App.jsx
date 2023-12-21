0dcda1034942bcebca270ab1055fc380846efa73
maddie <lee.maddie@gmail.com>
2023-12-21 09:40:38 -0500

    Set modalOpen state and toggleModal setter and pass through as props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  }
+
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute toggleModal={toggleModal} />
+      {modalOpen && <PhotoDetailsModal toggleModal={toggleModal} />}
     </div>
   );
 };
