2ceffc90ded22f18444bca832c8631aba2debbfc
maddie <lee.maddie@gmail.com>
2023-12-21 11:16:42 -0500

    Use setModalState to close modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,11 +4,11 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
-  const { activePhoto, toggleModal } = props;
+  const { activePhoto, setModalState } = props;
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" onClick={toggleModal} />
+        <img src={closeSymbol} alt="close symbol" onClick={() => setModalState(null)} />
       </button>
     </div>
   )
