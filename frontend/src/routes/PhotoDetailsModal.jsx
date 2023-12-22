9af803acdffb0f0b41aabcc3aee78220953f1f90
maddie <lee.maddie@gmail.com>
2023-12-22 11:15:56 -0500

    Arrange div order to use classes to centre top image

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -13,15 +13,18 @@ const PhotoDetailsModal = (props) => {
       <button className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" onClick={() => setModalState(null)} />
       </button>
+      
+      {/* Unsure of how to use classes here to centre photo */}
+      <div className='photo-details-modal__top-bar'>
+        <div className='photo-details-modal__header'>
+          <PhotoFavButton likeItem={null} photo={activePhoto} />
+          <img className='photo-details-modal__image' src={activePhoto.urls.full} />
 
-      <div className='photo-details-modal__header'>
-        <PhotoFavButton likeItem={null} photo={activePhoto} />
-        <img className='photo-details-modal__image' src={activePhoto.urls.full} />
-
-        <div className='photo-details-modal__photographer-details'>
-          <img className="photo-details-modal__photographer-profile" src={activePhoto.user.profile} />
-          <p className="photo-details-modal__photographer-info">{activePhoto.user.username}<br />
-          <span className="photo-details-modal__photographer-location">{`${activePhoto.location.city}, ${activePhoto.location.country}`}</span></p> 
+          <div className='photo-details-modal__photographer-details'>
+            <img className="photo-details-modal__photographer-profile" src={activePhoto.user.profile} />
+            <p className="photo-details-modal__photographer-info">{activePhoto.user.username}<br />
+            <span className="photo-details-modal__photographer-location">{`${activePhoto.location.city}, ${activePhoto.location.country}`}</span></p> 
+          </div>
         </div>
       </div>
         
