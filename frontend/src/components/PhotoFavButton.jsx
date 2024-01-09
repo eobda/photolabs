328dbebf9e772b56c8227ff2c14fa6d984b60dc1
maddie <lee.maddie@gmail.com>
2024-01-09 11:10:14 -0500

    Implement useEffect to ensure component is re-rendered across routes (HomeRoute when set in Modal and vice versa)

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
@@ -8,11 +8,15 @@ function PhotoFavButton(props) {
   const { photo, favPhotos, updateToFavPhotoIds, checkIfFav } = props;
 
   const [selected, setSelected] = useState(checkIfFav(photo.id, favPhotos));
+
   const handleClick = () => {
     updateToFavPhotoIds(photo, selected);
-    setSelected(selected ? false : true);
   };
   
+  useEffect(() => {
+    setSelected(checkIfFav(photo.id, favPhotos));
+  }, [favPhotos]);
+
   return (
     <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
