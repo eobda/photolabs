9bf065c0e9653a39eab541be18de4eb875817a65
maddie <lee.maddie@gmail.com>
2024-01-15 09:28:18 -0500

    Add HTML to return from PhotoDetailsModal

--- frontend/src/routes/LikedPhotosModal.jsx
+++ frontend/src/routes/LikedPhotosModal.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
-const LikedPhotosModal = (props) => {};
+import '../styles/PhotoDetailsModal.scss';
+
+const LikedPhotosModal = (props) => {
+  return (
+    <div className="photo-details-modal">
+
+    <div className='photo-details-modal__top-bar'>
+      <button className="photo-details-modal__close-button">
+        <img src={closeSymbol} alt="close symbol" onClick={() => onClosePhotoDetailsModal()} />
+      </button>
+    </div>
+              
+    <div className='photo-details-modal__images'>
+      <PhotoList
+        photoData={activePhoto.similar_photos}
+        favPhotos={state.favPhotos}
+        updateToFavPhotoIds={updateToFavPhotoIds}
+        onPhotoSelect={onPhotoSelect}
+      />
+    </div>
+
+  </div>
+  )
+};
 
 export default LikedPhotosModal;
