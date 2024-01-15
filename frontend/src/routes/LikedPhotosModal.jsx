5035ee63a41219c056c77b5dfca544c1ed627010
maddie <lee.maddie@gmail.com>
2024-01-15 09:31:20 -0500

    Import components

--- frontend/src/routes/LikedPhotosModal.jsx
+++ frontend/src/routes/LikedPhotosModal.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import '../styles/PhotoDetailsModal.scss';
+import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from "components/PhotoList";
 
 const LikedPhotosModal = (props) => {
   const {
